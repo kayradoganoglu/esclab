6d64b1bd14b2b555bb66f8317857e15d58314268
Kayra Doganoglu <kayradoganoglu2003@gmail.com>
2025-07-31 13:57:46 +0300

    data_process.py
    1. The code was corrected.
    2. Crop Function was restored to its normal state.

--- esclab/data_process.py
+++ esclab/data_process.py
@@ -3,36 +3,28 @@ from abstraction import EscData, take_values_from_csv
 
 
 class PostProcess(EscData):
-    def __init__(self, esc_data, type=0, esc_id=0, pole_value=23):
-        super().__init__(
-            esc_data.voltage, esc_data.current, esc_data.temp, esc_data.e_rpm,
-            esc_data.t_duty, esc_data.m_duty, esc_data.phase_current, esc_data.pwr,
-            esc_data.stat_1, esc_data.stat_2, esc_data.serial_number
-        )
+    def __init__(self, esc_data,type=0,esc_id=0,pole_value=23):
+        super().__init__(esc_data.voltage, esc_data.current, esc_data.temp, esc_data.e_rpm, esc_data.t_duty,
+                         esc_data.m_duty, esc_data.phase_current, esc_data.pwr, esc_data.stat_1, esc_data.stat_2,esc_data.serial_number)
         self.rpm = self.compute_rpm(pole_value)
-        self.running_array = [
-            (1, 0, 0, 0, 0, 1, 0, 1, 0, 1),
-            (0, 1, 0, 0, 1, 0, 1, 0, 1, 1),
-            (0, 0, 1, 0, 1, 1, 0, 0, 1, 1),
-            (0, 0, 0, 1, 0, 0, 1, 1, 1, 0)
-        ]
-        self.zero_crossing = []
-        self.mean_rpm = []
-        self.mean_thr = []
-        self.mean_voltage = []
-        self.mean_current = []
-        self.mean_temp = []
-        self.mean_phase_current = []
-        self.mean_pwr = []
-        self.mean_m_duty = []
-        self.test_type = type
-
+        self.running_array=[(1,0,0,0,0,1,0,1,0,1),(0,1,0,0,1,0,1,0,1,1),(0,0,1,0,1,1,0,0,1,1),(0,0,0,1,0,0,1,1,1,0)]
+        self.zero_crossing=[]
+        self.mean_rpm=[]
+        self.mean_thr=[]
+        self.mean_voltage=[]
+        self.mean_current=[]
+        self.mean_temp=[]
+        self.mean_phase_current=[]
+        self.mean_pwr=[]
+        self.mean_m_duty=[]
+        self.test_type=type
         if type == 0:
-            self.t_duty.insert(0, 0)
+            self.t_duty.insert(0,0)
             self.synchronize_steps(self.detect_step_commands())
             self.crop_data()
         elif type == 1:
-            print("type==1", "esc_id:", esc_id)
+            #self.start_end_crop()
+            print("type==1","esc_id:",esc_id)
             self.find_zero_crossing()
             print(self.zero_crossing)
             self.combined_step_syncro(esc_id=esc_id)
@@ -45,12 +37,14 @@ class PostProcess(EscData):
             print("Error: Post Process Type Not recognized.")
 
     def start_end_crop(self):
-        for i, value in enumerate(self.t_duty):
+        # Crop from the start
+        for i ,value in enumerate(self.t_duty):
             if value <= 3:
                 self.t_duty[i] = 0
 
         for i, value in enumerate(self.t_duty):
             if value == 0:
+                # Crop all lists up to and including the first zero in t_duty
                 self.timestamp = self.timestamp[i:]
                 self.voltage = self.voltage[i:]
                 self.current = self.current[i:]
@@ -62,10 +56,11 @@ class PostProcess(EscData):
                 self.pwr = self.pwr[i:]
                 self.stat_1 = self.stat_1[i:]
                 self.stat_2 = self.stat_2[i:]
+            else:
                 break
-
         for i in range(len(self.t_duty) - 1, -1, -1):
             if self.t_duty[i] == 0:
+                # Crop all lists after the first zero from the end in t_duty
                 self.timestamp = self.timestamp[:i + 1]
                 self.voltage = self.voltage[:i + 1]
                 self.current = self.current[:i + 1]
@@ -77,15 +72,17 @@ class PostProcess(EscData):
                 self.pwr = self.pwr[:i + 1]
                 self.stat_1 = self.stat_1[:i + 1]
                 self.stat_2 = self.stat_2[:i + 1]
+            else:
                 break
-
-    def flight_syncro(self, duration_sec=100):
+    def flight_syncro(self,duration_sec=100):
         time, rpm, current, motor_duty, temp, throttle_duty, voltage = [], [], [], [], [], [], []
-        phase_cur, pow = [], []
-        step_start_idx, step_end_idx = self.zero_crossing[0]
+        phase_cur , pow = [],[]
+        (step_start_idx, step_end_idx) = self.zero_crossing[0]
 
         num_points = step_end_idx - step_start_idx + 1
+
         dt = duration_sec / num_points
+
         t_temp = np.arange(0, duration_sec, dt)
         time.extend(t_temp)
 
@@ -97,6 +94,7 @@ class PostProcess(EscData):
         voltage.extend(self.voltage[step_start_idx:step_end_idx + 1])
         phase_cur.extend(self.phase_current[step_start_idx:step_end_idx + 1])
         pow.extend(self.pwr[step_start_idx:step_end_idx + 1])
+
 
         self.rpm = rpm
         self.current = current
@@ -111,29 +109,35 @@ class PostProcess(EscData):
     def find_zero_crossing_flight(self):
         start_index = []
         end_index = []
+
         for i in range(1, len(self.timestamp)):
             if self.t_duty[i] == 0 and self.t_duty[i - 1] != 0:
-                start_index.append(i)
+                start_index.append(self.timestamp[i])
             elif self.t_duty[i] != 0 and self.t_duty[i - 1] == 0:
-                end_index.append(i)
+                end_index.append(self.timestamp[i])
         self.zero_crossing.append((end_index[-1], start_index[-1]))
 
     def combined_step_syncro(self, esc_id, step_duration_sec=35):
         time, rpm, current, motor_duty, temp, throttle_duty, voltage = [], [], [], [], [], [], []
-        phase_cur, pow = [], []
+        phase_cur , pow = [],[]
         running_arr = self.running_array[esc_id]
-        current_time = 0
-        j = 0
 
+        total_duration_sec = step_duration_sec * len(running_arr)
+
+        overall_dt = total_duration_sec / len(running_arr)
+
+        current_time = 0
+        j=0
         for i_stp, run in enumerate(running_arr):
-            if run:
-                step_start_idx, step_end_idx = self.zero_crossing[j]
-                j += 1
+            if run:  # Only process steps where the ESC is running
+                (step_start_idx, step_end_idx) = self.zero_crossing[j]
+                j+=1
                 num_points = step_end_idx - step_start_idx + 1
+
                 dt = step_duration_sec / num_points
+
                 t_temp = np.arange(i_stp * step_duration_sec, (i_stp + 1) * step_duration_sec, dt)
                 time.extend(t_temp)
-
                 rpm.extend(self.rpm[step_start_idx:step_end_idx + 1])
                 current.extend(self.current[step_start_idx:step_end_idx + 1])
                 motor_duty.extend(self.m_duty[step_start_idx:step_end_idx + 1])
@@ -143,20 +147,21 @@ class PostProcess(EscData):
                 phase_cur.extend(self.phase_current[step_start_idx:step_end_idx + 1])
                 pow.extend(self.pwr[step_start_idx:step_end_idx + 1])
 
-                current_time += step_duration_sec
-                time.append(current_time)
-                rpm.append(0)
-                current.append(0)
-                motor_duty.append(0)
-                temp.append(0)
-                throttle_duty.append(0)
-                voltage.append(0)
-                phase_cur.append(0)
-                pow.append(0)
+            j = 0
+            current_time += step_duration_sec
+            time.append(current_time)
+            rpm.append(0)
+            current.append(0)
+            motor_duty.append(0)
+            temp.append(0)
+            throttle_duty.append(0)
+            voltage.append(0)
+            phase_cur.append(0)
+            pow.append(0)
 
         if not running_arr[-1]:
             num_points = len(time) - len(rpm)
-            print("NumPoints", num_points)
+            print("NumPoints",num_points)
             if num_points > 0:
                 t_temp = np.linspace(current_time, current_time + step_duration_sec, num_points, endpoint=False)
                 time.extend(t_temp)
@@ -172,36 +177,37 @@ class PostProcess(EscData):
         self.voltage = voltage
         self.timestamp = time
         self.phase_current = phase_cur
-        self.pwr = pow
+        self.pwr=pow
 
         print(f"Final timestamp: {self.timestamp[-1]} seconds, Length {len(self.timestamp)}")
 
     def find_zero_crossing(self):
         start_index = []
         end_index = []
+
         for i in range(1, len(self.timestamp)):
             if self.t_duty[i] == 0 and self.t_duty[i - 1] != 0:
-                start_index.append(i)
+                start_index.append(self.timestamp[i])
             elif self.t_duty[i] != 0 and self.t_duty[i - 1] == 0:
-                end_index.append(i)
+                end_index.append(self.timestamp[i])
 
-        for i in range(len(end_index)):
-            self.zero_crossing.append((end_index[i], start_index[i + 1]))
+        for i in (range(len(end_index))):
+            self.zero_crossing.append((end_index[i], start_index[i+1]))
 
-    def compute_rpm(self, var=23):
+    def compute_rpm(self,var=23):
         return np.array(self.e_rpm) / var
 
     def crop_data(self):
-        del self.voltage[len(self.timestamp) - 1:]
-        del self.current[len(self.timestamp) - 1:]
-        del self.temp[len(self.timestamp) - 1:]
-        del self.rpm[len(self.timestamp) - 1:]
-        del self.t_duty[len(self.timestamp) - 1:]
-        del self.m_duty[len(self.timestamp) - 1:]
-        del self.phase_current[len(self.timestamp) - 1:]
-        del self.pwr[len(self.timestamp) - 1:]
-        del self.stat_1[len(self.timestamp) - 1:]
-        del self.stat_2[len(self.timestamp) - 1:]
+        del self.voltage[len(self.timestamp)-1:]
+        del self.current[len(self.timestamp)-1:]
+        del self.temp[len(self.timestamp)-1:]
+        del self.rpm[len(self.timestamp)-1:]
+        del self.t_duty[len(self.timestamp)-1:]
+        del self.m_duty[len(self.timestamp)-1:]
+        del self.phase_current[len(self.timestamp)-1:]
+        del self.pwr[len(self.timestamp)-1:]
+        del self.stat_1[len(self.timestamp)-1:]
+        del self.stat_2[len(self.timestamp)-1:]
         del self.timestamp[-1]
 
     def detect_step_commands(self, threshold=9, min_gap=5):
@@ -230,12 +236,16 @@ class PostProcess(EscData):
 
     def synchronize_steps(self, step_cmd_idx, step_duration_sec=5):
         time, rpm, current, motor_duty, temp, throttle_duty, voltage = [], [], [], [], [], [], []
-        phase_cur, pow = [], []
+        phase_cur,pow=[],[]
         steps = [(step_cmd_idx[i], step_cmd_idx[i + 1] - 1) for i in range(len(step_cmd_idx) - 1)]
+
         filtered_steps = self.detect_difference_pairs(steps)
 
-        for i_stp, (step_start_idx, step_end_idx) in enumerate(filtered_steps):
+        for i_stp in range(len(filtered_steps)):
+            step_start_idx, step_end_idx = filtered_steps[i_stp]
+
             dt = step_duration_sec / (step_end_idx - step_start_idx + 1)
+
             t_temp = np.arange(i_stp * step_duration_sec, (i_stp + 1) * step_duration_sec, dt)
             time.extend(t_temp)
 
@@ -258,7 +268,8 @@ class PostProcess(EscData):
         self.phase_current = phase_cur
         self.pwr = pow
 
-        for i_stp, (step_start_idx, step_end_idx) in enumerate(filtered_steps):
+        for i_stp in range(len(filtered_steps)):
+            step_start_idx, step_end_idx = filtered_steps[i_stp]
             self.mean_rpm.append(np.mean(self.rpm[step_start_idx:step_end_idx + 1]))
             self.mean_thr.append(np.mean(self.t_duty[step_start_idx:step_end_idx + 1]))
             self.mean_voltage.append(np.mean(self.voltage[step_start_idx:step_end_idx + 1]))
